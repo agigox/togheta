456b04e1b4f10da30a3ebc09fff5dbc383b3c3f6
Amine TABOU <amin.tabou@gmail.com>
2025-06-12 14:46:50 +0200

    Togheta init

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -1,25 +1,23 @@
-import { Text, View } from 'react-native';
+import { Text, Image, Pressable, View } from 'react-native';
 
-import { EditScreenInfo } from './EditScreenInfo';
+import { LineChart } from 'react-native-chart-kit';
 
-type ScreenContentProps = {
-  title: string;
-  path: string;
-  children?: React.ReactNode;
-};
-
-export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+export const ScreenContent = () => {
+  const onPressFunction = () => {
+    console.log('Button Pressed!');
+  };
   return (
-    <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
-      <EditScreenInfo path={path} />
-      {children}
+    <View className="flex flex-1 flex-col items-center justify-center gap-4 border bg-white">
+      <Text className="text-xl font-bold">Welcome to Togheta</Text>
+      <Image className="h-36 w-36 rounded-full" source={require('../assets/image.png')} />
+      <Pressable onPress={onPressFunction} className="rounded-lg bg-blue-500 px-4 py-2">
+        <Text className="text-white">I'm pressable!</Text>
+      </Pressable>
     </View>
   );
 };
 const styles = {
   container: `items-center flex-1 justify-center`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
+  title: `text-xl font-bold text-red-500`,
 };
