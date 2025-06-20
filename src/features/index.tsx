38e8e2fa1054be262d48191b513270ab7344a8a4
Amine TABOU <amin.tabou@gmail.com>
2025-06-20 11:45:36 +0200

    Adding Firebase v9 modular SDK and removing compat layer

--- src/features/index.tsx
+++ src/features/index.tsx
@@ -3,6 +3,19 @@ import AppLoading from 'expo-app-loading';
 import { AuthScreen } from '~/features/auth';
 import { useAuth } from '~/context/AuthContext';
 import { TaskScreen } from './tasks';
+import * as SecureStore from 'expo-secure-store';
+async function save(key: string, value: string) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+async function getValueFor(key: string) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    alert("🔐 Here's your value 🔐 \n" + result);
+  } else {
+    alert('No values stored under that key.');
+  }
+}
 
 export default function Features() {
   const { user, loading } = useAuth();
